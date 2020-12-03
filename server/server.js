b39b97f37861482b7c32d9b9ce8618f2af8e9b95
jesus.alonso <190313@utags.edu.mx>
2020-12-02 18:22:28 -0600

    Ambiete preparado para uso de Nodejs y base de datos

--- server/server.js
+++ server/server.js
@@ -17,15 +17,15 @@ app.get('/', function (req, res) {
 app.use(require('./routes/usuario'));
 
 
-//mongoose.connect('mongodb://localhost:27017/cafeteria', {
-//   //  useNewUrlParser: true,
-//   //useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true
-// }, (err, res ) =>  {
-//   if (err) throw error;
-//   console.log('Base de datos ONLINE');
-// /});
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+  useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+ useCreateIndex: true
+ }, (err, res ) =>  {
+   if (err) throw error;
+ console.log('Base de datos ONLINE');
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor esta en liena en el puerto ', process.env.PORT);
