b7cc5ff19431ef55177a4cea19b4d00580daec3e
jesus.alonso <190313@utags.edu.mx>
2020-12-04 13:50:08 -0600

    Relizacion de la tabla Productos, con GET, PUT, POST y DELETE

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
-// parse application/json
+// parse application/json 
 app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
@@ -17,6 +17,7 @@ app.get('/', function (req, res) {
 app.use(require('./routes/usuario'));
 app.use(require('./routes/categoria'));
 app.use(require('./routes/login'));
+app.use(require('./routes/producto'));
 
 mongoose.connect('mongodb://localhost:27017/cafeteria', {
   useNewUrlParser: true,
